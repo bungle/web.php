24814be64c3ca159f557445bcd632ebcc59cb2df
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2012-02-22 01:12:05 +0200

    Added support for less strict passwords (something that Facebook does).

--- password.php
+++ password.php
@@ -1,13 +1,13 @@
 <?php
 namespace password;
 function hash($password, $iterations = 8) {
-  // Password (PHP >= 5.3 version of http://www.openwall.com/phpass/)
+    // Password (PHP >= 5.3 version of http://www.openwall.com/phpass/)
     if (function_exists('mcrypt_create_iv')) {
         $random = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
     } else {
         if (is_readable('/dev/urandom') && ($fh = @fopen('/dev/urandom', 'rb'))) {
-                $random = fread($fh, 16);
-                fclose($fh);
+            $random = fread($fh, 16);
+            fclose($fh);
         }
         if (strlen($random) < 16) {
             $random = '';
@@ -19,31 +19,34 @@ function hash($password, $iterations = 8) {
             $random = substr($random, 0, 16);
         }
     }
-  $itoa64 = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  if ($iterations < 4 || $iterations > 31) $iterations = 8;
-  $salt = '$2a$';
-  $salt .= chr(ord('0') + $iterations / 10);
-  $salt .= chr(ord('0') + $iterations % 10);
-  $salt .= '$';
-  $i = 0;
-  calc:
-  $c1 = ord($random[$i++]);
-  $salt .= $itoa64[$c1 >> 2];
-  $c1 = ($c1 & 0x03) << 4;
-  if ($i >= 16) {
+    $itoa64 = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    if ($iterations < 4 || $iterations > 31) $iterations = 8;
+    $salt = '$2a$';
+    $salt .= chr(ord('0') + $iterations / 10);
+    $salt .= chr(ord('0') + $iterations % 10);
+    $salt .= '$';
+    $i = 0;
+    calc:
+    $c1 = ord($random[$i++]);
+    $salt .= $itoa64[$c1 >> 2];
+    $c1 = ($c1 & 0x03) << 4;
+    if ($i >= 16) {
+        $salt .= $itoa64[$c1];
+        return crypt($password, $salt);
+    }
+    $c2 = ord($random[$i++]);
+    $c1 |= $c2 >> 4;
     $salt .= $itoa64[$c1];
-    return crypt($password, $salt);
-  }
-  $c2 = ord($random[$i++]);
-  $c1 |= $c2 >> 4;
-  $salt .= $itoa64[$c1];
-  $c1 = ($c2 & 0x0f) << 2;
-  $c2 = ord($random[$i++]);
-  $c1 |= $c2 >> 6;
-  $salt .= $itoa64[$c1];
-  $salt .= $itoa64[$c2 & 0x3f];
-  goto calc;
+    $c1 = ($c2 & 0x0f) << 2;
+    $c2 = ord($random[$i++]);
+    $c1 |= $c2 >> 6;
+    $salt .= $itoa64[$c1];
+    $salt .= $itoa64[$c2 & 0x3f];
+    goto calc;
 }
-function check($password, $hash) {
-  return crypt($password, $hash) === $hash;
+function check($password, $hash, $strict = true) {
+    $valid = crypt($password, $hash) === $hash;
+    if ($strict || $valid) return $valid;
+    return crypt(mb_strtolower($password, 'UTF-8') ^ mb_strtoupper($password, 'UTF-8') ^ $password, $hash) === $hash ||
+           crypt(mb_strtoupper(mb_substr($password, 0, 1, 'UTF-8')) . mb_substr($password, 1, mb_strlen($password, 'UTF-8'), 'UTF-8'), $hash) === $hash;
 }
