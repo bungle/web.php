9cb5b1798ea6422e67822cbf2154a7f14c18c12c
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2012-11-26 17:41:32 +0200

    Added syslog.

--- log.php
+++ log.php
@@ -7,11 +7,14 @@ namespace log {
         $args = func_get_args();
         foreach ($args as $arg) $appenders[] = $arg;
     }
-    function debug($message) { append($message, LOG_DEBUG, debug_backtrace()); }
-    function info($message) { append($message, LOG_INFO, debug_backtrace()); }
-    function warn($message) { append($message, LOG_WARNING, debug_backtrace()); }
-    function error($message) { append($message, LOG_ERR, debug_backtrace()); }
-    function append($message, $level, $trace) {
+    function debug() { append(func_get_args(), LOG_DEBUG,   debug_backtrace()); }
+    function info()  { append(func_get_args(), LOG_INFO,    debug_backtrace()); }
+    function warn()  { append(func_get_args(), LOG_WARNING, debug_backtrace()); }
+    function error() { append(func_get_args(), LOG_ERR,     debug_backtrace()); }
+    function append($args, $level, $trace) {
+        $message = '';
+        if (count($args) === 1)   $message = $args[0];
+        elseif (count($args) > 1) $message = sprintf(array_shift($args), $args);
         $appenders = appenders();
         foreach ($appenders as $appender) $appender($message, $level, $trace);
     }
@@ -22,7 +25,7 @@ namespace log {
     }
     function file($file, $log_level = LOG_DEBUG) {
         return function($message, $level, $trace) use ($file, $log_level) {
-            if ($log_level < $level) return;
+            if ($log_level < $level) return false;
             static $messages = null;
             if ($messages == null) {
                 register_shutdown_function(function($file) use (&$messages) {
@@ -34,9 +37,19 @@ namespace log {
             $messages .= sprintf('%s %7s %-20s %s', date('Y-m-d H:i:s.', $sec) . substr($usec, 2, 3) , level($level), basename($trace[0]['file']) . ':' . $trace[0]['line'], trim($message) . PHP_EOL);
         };
     }
-    function chromephp($log_level) {
+    function syslog($log_level = LOG_DEBUG, $ident = false, $option = LOG_ODELAY, $facility = LOG_USER) {
+        return function($message, $level, $trace) use ($log_level, $ident, $option, $facility) {
+            if ($log_level < $level) return false;
+            if ($ident === false && $option === LOG_ODELAY && $facility === LOG_USER) return syslog($level, $message);
+            if (openlog($ident, $option, $facility) === false) return false;
+            $ret = syslog($level, $message);
+            closelog();
+            return $ret;
+        };
+    }
+    function chromephp($log_level = LOG_DEBUG) {
         return function($message, $level, $trace) use ($log_level) {
-            if ($log_level < $level) return;
+            if ($log_level < $level) return false;
             switch ($level) {
                 case LOG_ERR: return \ChromePhp::error($message);
                 case LOG_WARNING: return \ChromePhp::warn($message);
