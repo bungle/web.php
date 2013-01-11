7137c98a47073e3202a2d837caba708d9f1ec080
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2013-01-11 11:42:14 +0200

    Removed useless status-function. Use header(':', true, <code>); or (PHP 5.4+) http_response_code(<code>) instead.

--- web.php
+++ web.php
@@ -70,63 +70,6 @@ function accept() {
     header("Content-type: ${type}");
     return $func;
 }
-function status($code) {
-    switch ($code) {
-        // Informational
-        case 100: $msg = 'Continue'; break;
-        case 101: $msg = 'Switching Protocols'; break;
-        // Successful
-        case 200: $msg = 'OK'; break;
-        case 201: $msg = 'Created'; break;
-        case 202: $msg = 'Accepted'; break;
-        case 203: $msg = 'Non-Authoritative Information'; break;
-        case 204: $msg = 'No Content'; break;
-        case 205: $msg = 'Reset Content'; break;
-        case 206: $msg = 'Partial Content'; break;
-        // Redirection
-        case 300: $msg = 'Multiple Choices'; break;
-        case 301: $msg = 'Moved Permanently'; break;
-        case 302: $msg = 'Found'; break;
-        case 303: $msg = 'See Other'; break;
-        case 304: $msg = 'Not Modified'; break;
-        case 305: $msg = 'Use Proxy'; break;
-        case 306: $msg = '(Unused)'; break;
-        case 307: $msg = 'Temporary Redirect'; break;
-        // Client Error
-        case 400: $msg = 'Bad Request'; break;
-        case 401: $msg = 'Unauthorized'; break;
-        case 402: $msg = 'Payment Required'; break;
-        case 403: $msg = 'Forbidden'; break;
-        case 404: $msg = 'Not Found'; break;
-        case 405: $msg = 'Method Not Allowed'; break;
-        case 406: $msg = 'Not Acceptable'; break;
-        case 407: $msg = 'Proxy Authentication Required'; break;
-        case 408: $msg = 'Request Timeout'; break;
-        case 409: $msg = 'Conflict'; break;
-        case 410: $msg = 'Gone'; break;
-        case 411: $msg = 'Length Required'; break;
-        case 412: $msg = 'Precondition Failed'; break;
-        case 413: $msg = 'Request Entity Too Large'; break;
-        case 414: $msg = 'Request-URI Too Long'; break;
-        case 415: $msg = 'Unsupported Media Type'; break;
-        case 416: $msg = 'Requested Range Not Satisfiable'; break;
-        case 417: $msg = 'Expectation Failed'; break;
-        case 428: $msg = 'Precondition Required'; break;
-        case 429: $msg = 'Too Many Requests'; break;
-        case 431: $msg = 'Request Header Fields Too Large'; break;
-        // Server Error
-        case 500: $msg = 'Internal Server Error'; break;
-        case 501: $msg = 'Not Implemented'; break;
-        case 502: $msg = 'Bad Gateway'; break;
-        case 503: $msg = 'Service Unavailable'; break;
-        case 504: $msg = 'Gateway Timeout'; break;
-        case 505: $msg = 'HTTP Version Not Supported'; break;
-        case 511: $msg = 'Network Authentication Required'; break;
-        default: return;
-    }
-    $protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.1';
-    header("$protocol $code $msg");
-}
 function url($url = null, $abs = false) {
     if ($url == null) {
         $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
